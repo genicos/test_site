a51214db72c2b11443294d13e336ecdc6d8fc198
Nicolas Ayala <nicoatnico.com@gmail.com>
2022-05-16 15:57:50 -0700

    Updated files.

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function on_click(){
+    console.log("her")
+
+    names_string = [0,0,0]
+    score_string = [29,39,01]
+
+    fetch("https://7320-2601-647-cb00-4c50-4cae-f0ca-4379-8731.ngrok.io/yuh", {
+        headers: {
+            Accept: "application/json",
+            'Content-Type': 'application/json',
+            origin: "/wsl%24/Ubuntu/home/genico/practice/site/index.html"
+        },
+        method: "POST",
+        body: JSON.stringify({
+            "names": names_string,
+            "scores": score_string
+        })
+    }).then(res => {
+        console.log("Request complete! response:", res);
+    });
+}
